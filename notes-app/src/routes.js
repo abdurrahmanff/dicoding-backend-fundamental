cc261a197ffe8cab7ef96c0ff300ba9a833a90bc
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-14 15:19:50 +0700

    init

---
+++ notes-app/src/routes.js
@@ -0,0 +1,37 @@
+import {
+  saveBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} from './handler.js';
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: saveBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+export default routes;
---
+++ notes-app/src/server.js
@@ -0,0 +1,14 @@
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+
+  server.route(routes);
+  await server.start();
+};
+
+init();
