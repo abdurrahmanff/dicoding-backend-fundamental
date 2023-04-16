68dfcad325e8efb02ce5bb0e0e26b3124fc5c223
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-16 13:00:34 +0700

    feat: implements validation

---
+++ notes-app/src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require('../../exceptions/InvariantError.js');
+const NotePayloadSchema = require('./schema.js');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
