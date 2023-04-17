d878981ef757d6e68a1631a272159d0a9c24ac9f
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-18 02:22:41 +0700

    feat: create songs table migration

---
+++ openmusic/migrations/1681758404333_create-table-songs.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true
+    },
+    title: {
+      type: 'VARCHAR(100)',
+      notNull: true
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    performer: {
+      type: 'VARCHAR(50)',
+      notNull: true
+    },
+    genre: {
+      type: 'VARCHAR(30)',
+      notNull: true
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: false
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: false
+    },
+    created_at: {
+      type: 'VARCHAR(30)',
+      notNull: true
+    },
+    updated_at: {
+      type: 'VARCHAR(30)',
+      notNull: true,
+    }
+  })
+};
+
+exports.down = pgm => {
+  pgm.dropTable('songs')
+};
