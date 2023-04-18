b0e7c5b4ac27a96a9bc98ffe4d7b2d9f08c01f8e
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-18 11:47:46 +0700

    feat: register album plugin

--- openmusic/src/server.js
+++ openmusic/src/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const AlbumsService = require('./services/AlbumsService');
+const albums = require('./api/albums');
+const AlbumValidator = require('./validator/album');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const server = Hapi.server({
@@ -13,7 +17,48 @@ const init = async () => {
     },
   });
 
+  const albumsService = new AlbumsService();
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
+
   await server.start();
+  // eslint-disable-next-line no-console
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
