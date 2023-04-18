03c0544d25ab5e7a973d0cbcf6a3ab7728e96cc4
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-18 19:08:50 +0700

    fix: wrong plugin register

--- notes-app/src/api/notes/index.js
+++ notes-app/src/api/notes/index.js
@@ -1,11 +1,11 @@
 const routes = require('./routes.js');
-const UsersHandler = require('./handler.js');
+const NotesHandler = require('./handler.js');
 
 module.exports = {
   name: 'notes',
   version: '1.0.0',
   register: async (server, { service, validator }) => {
-    const usersHandler = new UsersHandler(service, validator);
-    server.route(routes(usersHandler));
+    const notesHandler = new NotesHandler(service, validator);
+    server.route(routes(notesHandler));
   },
 };
