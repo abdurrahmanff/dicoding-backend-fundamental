3a2d97e3bd150ccbcc50f29875daa1a40986e6c3
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-18 18:31:45 +0700

    feat: implements user payload validator

---
+++ notes-app/src/validator/users/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require('../../exceptions/InvariantError.js');
+const UserPayloadSchema = require('./schema.js');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validationResult = UserPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
