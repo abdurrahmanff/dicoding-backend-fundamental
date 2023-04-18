7299880dbad27eb1c7618c025efb5b5ba4517c7d
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-18 11:08:56 +0700

    feat: implement albums service

---
+++ openmusic/src/services/AlbumsService.js
@@ -0,0 +1,70 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const { MapAlbumDbToModel } = require('../utils');
+
+class AlbumsService {
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, name, year, createdAt, updatedAt],
+    };
+
+    const result = await this.pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new Error('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT name, year FROM albums WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    if (!result.rowCount) {
+      throw Error('Album tidak ditemukan');
+    }
+
+    return result.rows.map(MapAlbumDbToModel)[0];
+  }
+
+  async editAlbumById(id, { name, year }) {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: 'UPDATE albums SET name = $2, year = $3, updated_at = $4 WHERE id = $1 RETURNING id',
+      values: [id, name, year, updatedAt],
+    };
+
+    const result = await this.pool.query(query);
+    if (!result.rowCount) {
+      throw new Error('Gagal memperbarui album. Id tidak ditemukan');
+    }
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    if (!result.rowCount) {
+      throw new Error('Gagal menghapus album. Id tidak ditemukan');
+    }
+  }
+}
+
+module.exports = AlbumsService;
