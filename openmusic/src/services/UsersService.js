2f613f46be1bebc18f6edf25ac8dc780f36b2117
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-19 21:29:52 +0700

    feat: add users service

---
+++ openmusic/src/services/UsersService.js
@@ -0,0 +1,40 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../exceptions/InvariantError');
+
+class UsersService {
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT into users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this.pool.query(query);
+
+    if (!result.row[0].id) {
+      throw new InvariantError('Gagal menambahkan user');
+    }
+    return result.row[0].id;
+  }
+
+  async verifyNewUser(username) {
+    const query = {
+      text: 'SELECT id FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this.pool.query(query);
+    if (result.rowCount) {
+      throw new InvariantError('Gagal menambahkan user. Username telah digunakan');
+    }
+  }
+}
+
+module.exports = UsersService;
