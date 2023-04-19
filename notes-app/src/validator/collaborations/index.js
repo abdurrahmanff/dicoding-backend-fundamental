5989096494c0bcf60cf601dc078a99afc96bb6a8
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-19 11:45:49 +0700

    feat: add collab schema

---
+++ notes-app/src/validator/collaborations/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError.js');
+const { CollaborationPayloadSchema } = require('./schema.js');
+
+const CollaborationsValidator = {
+  validateCollaborationPayload: (payload) => {
+    const validationResult = CollaborationPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = CollaborationsValidator;
