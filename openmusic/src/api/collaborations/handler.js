baea1a2926bd89be7eded41a6291ae8e1e34e4dd
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-04-29 16:36:24 +0700

    feat: implements collab plugin

---
+++ openmusic/src/api/collaborations/handler.js
@@ -0,0 +1,50 @@
+const autoBind = require('auto-bind');
+
+class CollaborationsHandler {
+  constructor(collaborationsService, playlistService, validator) {
+    this.collaborationsService = collaborationsService;
+    this.playlistService = playlistService;
+    this.validator = validator;
+
+    autoBind(this);
+  }
+
+  async postCollaborationHandler(request, h) {
+    this.validator.validateCollaborationPayload(request.payload);
+
+    const { id: credentialId } = request.auth.credentials;
+    const { playlistId, userId } = request.payload;
+
+    await this.playlistService.verifyPlaylistOwner(playlistId, { owner: credentialId });
+    const collaborationId = await this.collaborationsService.addCollaboration({
+      playlistId, userId,
+    });
+
+    const response = h.response({
+      status: 'message',
+      message: 'Berhasil menambahkan kolaborasi',
+      data: {
+        collaborationId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async deleteCollaborationhandler(request) {
+    this.validator.validateCollaborationPayload(request.payload);
+
+    const { id: credentialId } = request.auth.credentials;
+    const { playlistId, userId } = request.payload;
+
+    await this.playlistService.verifyPlaylistOwner(playlistId, { owner: credentialId });
+    await this.collaborationsService.removeCollaboration({ playlistId, userId });
+
+    return {
+      status: 'success',
+      message: 'Berhasil menghapus kolaborasi',
+    };
+  }
+}
+
+module.exports = CollaborationsHandler;
---
+++ openmusic/src/validator/collaboration/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { CollaborationSchema } = require('./schema');
+
+const CollaborationValidator = {
+  validateCollaborationPayload: (payload) => {
+    const validationResult = CollaborationSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = CollaborationValidator;
