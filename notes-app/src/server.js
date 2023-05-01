14e0510ae8de9c06005e4a3c8ae98aaba4dddda4
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-01 19:41:52 +0700

    feat: register uploads plugin

--- notes-app/src/server.js
+++ notes-app/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const path = require('path');
 
 // notes
 const notes = require('./api/notes/index.js');
@@ -29,11 +30,17 @@ const _exports = require('./api/exports/index.js');
 const ProducerService = require('./services/rabbitmq/ProducerService.js');
 const ExportsValidator = require('./validator/exports/index.js');
 
+// uploads
+const uploads = require('./api/uploads/index.js');
+const StorageService = require('./services/storage/StorageService.js');
+const UploadsValidator = require('./validator/uploads/index.js');
+
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -106,6 +113,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
