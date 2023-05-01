5091e7ca2969ed6f872383e65761bb0513a1e3e5
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-01 19:38:08 +0700

    feat: implements upload payload validator

---
+++ notes-app/src/validator/uploads/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError.js');
+const { ImageHeadersSchema } = require('./schema.js');
+
+const UploadsValidator = {
+  validateImageHeaders: (headers) => {
+    const validationResult = ImageHeadersSchema.validate(headers);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UploadsValidator;
