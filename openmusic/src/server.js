82a865e29e475721b235717c063c7d2fc519051f
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-02 20:37:06 +0700

    feat: register exports plugin

--- openmusic/src/server.js
+++ openmusic/src/server.js
@@ -34,6 +34,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaboration');
 
+// Exports plugin
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
@@ -123,6 +128,14 @@ const init = async () => {
         playlistsService,
         usersService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        ProducerService,
+        playlistsService,
+        validator: ExportsValidator,
       },
     },
   ]);
