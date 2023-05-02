8691dbfc60bf8574cc95c34f626dccbf760f545c
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-02 20:30:27 +0700

    feat: implements exports plugin

---
+++ openmusic/src/api/exports/handler.js
@@ -0,0 +1,31 @@
+class ExportsHandler {
+  constructor(exportsService, playlistsService, validator) {
+    this.exportsService = exportsService;
+    this.playlistsService = playlistsService;
+    this.validator = validator;
+  }
+
+  async postExportPlaylistByIdHandler(request, h) {
+    this.validator.validateExportPlaylistPayload(request.payload);
+
+    const { playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+    await this.playlistsService.verivyPlaylistOwner(playlistId, { owner: credentialId });
+
+    const message = {
+      userId: credentialId,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this.exportsService.sendMessage('export:playlists', message);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
