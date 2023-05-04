d45fa6f8c12c11e81ad0f7c0d205d8748c1c18bd
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-04 13:48:02 +0700

    fix: forgot route options

--- openmusic/src/api/albums/routes.js
+++ openmusic/src/api/albums/routes.js
@@ -23,6 +23,14 @@ const routes = (handler) => [
     method: 'POST',
     path: '/albums/{id}/covers',
     handler: handler.postAlbumCoverByIdHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true,
+        output: 'stream',
+        maxBytes: 512000,
+      },
+    },
   },
 ];
 
